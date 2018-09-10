2aeaef858de70b0f407fc27f41b16a1102840a34
Jordan Moreno-Lacalle <jordanmlacalle@gmail.com>
2018-09-10 10:51:12 -0400

    Add tentative classes
    Added TransacionClassifier and Dataset classes. Main method is present in TransactionClassifier class.

---
+++ src/com/jordanml/TransactionClassifier/Dataset.java
@@ -0,0 +1,5 @@
+package com.jordanml.TransactionClassifier;
+
+public class Dataset {
+
+}
---
+++ src/com/jordanml/TransactionClassifier/TransactionClassifier.java
@@ -0,0 +1,9 @@
+package com.jordanml.TransactionClassifier;
+
+public class TransactionClassifier {
+    
+    public static void main(String args[]) {
+        
+    }
+
+}
