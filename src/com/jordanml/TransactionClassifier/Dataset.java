9085e41e8bdc27cc2f2f1bbb12f11e41bccb94c9
Jordan Moreno-Lacalle <jordanmlacalle@gmail.com>
2018-09-10 11:16:35 -0400

    Add more comments

--- src/com/jordanml/TransactionClassifier/Dataset.java
+++ src/com/jordanml/TransactionClassifier/Dataset.java
@@ -5,11 +5,22 @@ import weka.core.converters.ConverterUtils.DataSource;
 
 public class Dataset {
     
+    /**
+     * data - the object that contains the dataset 
+     * path - the path to the file from which the dataset was loaded
+     */
     public Instances data = null;
     public String path = null;
     
+    /**
+     * Constructor
+     */
     public Dataset() {}
-
+    
+    /**
+     * Constructor with path as parameter. Loads dataset when new Dataset object is created.
+     * @param path The path to the file containing the dataset.
+     */
     public Dataset(String path) {
         this.path = path;
         loadData(path);
