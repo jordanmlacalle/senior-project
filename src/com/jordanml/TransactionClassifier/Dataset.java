9033c437fa15aa8941a3ef1d8b51a0c2c4bd7310
Jordan Moreno-Lacalle <jordanmlacalle@gmail.com>
2018-09-10 11:13:33 -0400

    Add dataset loading functionality to Dataset class
    The loadData method loads a dataset from the specified path to the data object

--- src/com/jordanml/TransactionClassifier/Dataset.java
+++ src/com/jordanml/TransactionClassifier/Dataset.java
@@ -1,5 +1,36 @@
 package com.jordanml.TransactionClassifier;
 
-public class Dataset {
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
 
+public class Dataset {
+    
+    public Instances data = null;
+    public String path = null;
+    
+    public Dataset() {}
+
+    public Dataset(String path) {
+        this.path = path;
+        loadData(path);
+    }
+    
+    /**
+     * Loads dataset from the provided file into the data Instances object.
+     * 
+     * @param path The path to the file containing the dataset.
+     */
+    public void loadData(String path) {
+        
+        System.out.println("Loading data from " + path + "...");
+        
+        try {
+            //See p.210 of the WEKA 3-8-2 manual
+            data = DataSource.read(path);
+        } catch (Exception e) {
+            System.out.println("Error reading " + path);
+            System.out.println(e.getMessage());
+        }
+    }
+    
 }
